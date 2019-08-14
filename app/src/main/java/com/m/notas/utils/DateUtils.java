313778be817da6d8d72f1aa7c328a1a1c637c29e
soumarcelino <marcelinomatiasdesouza@gmail.com>
2019-08-13 22:32:35 -0300

    Add format time util

--- app/src/main/java/com/m/notas/utils/DateUtils.java
+++ app/src/main/java/com/m/notas/utils/DateUtils.java
@@ -1,20 +1,13 @@
 package com.m.notas.utils;
 
+import org.ocpsoft.prettytime.PrettyTime;
+
 import java.util.Date;
-import java.text.SimpleDateFormat;
-import java.util.Locale;
 
 public class DateUtils {
 
-    public static String toString(Date date){
-        SimpleDateFormat sdf;
-        if(Locale.getDefault().getDisplayLanguage().equals("português")){
-            sdf = new SimpleDateFormat(Constants.TIME_STAMP_FORMAT_PT);
-        } else {
-            sdf = new SimpleDateFormat(Constants.TIME_STAMP_FORMAT_DEFAULT);
-            System.out.println(Locale.getDefault().getDisplayLanguage());
-        }
-
-        return sdf.format(date);
+    public static String prettyTime(Date date){
+        PrettyTime prettyTime = new PrettyTime();
+        return prettyTime.format(date);
     }
 }
